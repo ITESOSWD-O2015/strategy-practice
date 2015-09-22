ef9980b892866433ca162b66ce5c2d1ac551f7a3
Aldo Ruiz <is692909@iteso.mx>
2015-09-22 08:51:54 -0500

    Tarea parte 3

--- src/main/java/JamaiconSportsLifesaversModule.java
+++ src/main/java/JamaiconSportsLifesaversModule.java
@@ -16,22 +16,22 @@ class JamaiconSportsLifesaversModule {
         Lifesaver armBands = new ArmBands();
 
         System.out.println(swimRing.display());
-        System.out.println(swimRing.inflating());
-        System.out.println(swimRing.deflating());
+        System.out.println(swimRing.performInflate());
+        System.out.println(swimRing.performDeflate());
         System.out.println(swimRing.floating());
 
         System.out.println();
 
         System.out.println(swimSeat.display());
-        System.out.println(swimSeat.inflating());
-        System.out.println(swimSeat.deflating());
+        System.out.println(swimSeat.performInflate());
+        System.out.println(swimSeat.performDeflate());
         System.out.println(swimSeat.floating());
 
         System.out.println();
 
         System.out.println(armBands.display());
-        System.out.println(armBands.inflating());
-        System.out.println(armBands.deflating());
+        System.out.println(armBands.performInflate());
+        System.out.println(armBands.performDeflate());
         System.out.println(armBands.floating());
     }
 }
